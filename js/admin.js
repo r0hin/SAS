fbee4daa08f30211ffbc38038d4f5c83e0c31168
rohinarya12 <46874702+rohinarya12@users.noreply.github.com>
2020-03-30 03:34:05 -0400

    update

--- js/admin.js
+++ js/admin.js
@@ -1,3 +1,4 @@
+
 // Your web app's Firebase configuration
 var firebaseConfig = {
     apiKey: "AIzaSyBQkNbhM7YZhge0QTLt6jCNGBy6vF6cpnQ",
@@ -30,12 +31,146 @@ firebase.auth().onAuthStateChanged(function (user) {
 
 
 function setup() {
-    console.log('aok');
+    loadsets()
     document.getElementById('signed').style.display = 'block'
 }
 
-$('#setmodal').modal('toggle')
 
 function createset() {
 
+    name = document.getElementById('set1').value
+    notes = document.getElementById('set2').value
+    rest = document.getElementById('set3').value
+    sets = document.getElementById('set4').value
+    reps = document.getElementById('set5').value
+    rir = document.getElementById('set6').value
+    day = document.getElementById('set7').value
+
+    db.collection('sets').add({
+        name: name,
+        notes: notes,
+        rest: rest,
+        sets: sets,
+        reps: reps,
+        rir: rir,
+        day: day
+    }).then(function () {
+        Snackbar.show({ text: 'Set created.' })
+        refresh()
+    })
+
+
 }
+
+function refresh() {
+    $('#sets').empty()
+    loadsets()
+}
+
+function prepareedit(id) {
+
+    document.getElementById('editbutton').onclick = function () {
+        edit(id)
+    }
+
+    db.collection('sets').doc(id).get().then(function (doc) {
+        document.getElementById('newset1').value = doc.data().name
+        document.getElementById('newset2').value = doc.data().notes
+        document.getElementById('newset3').value = doc.data().rest
+        document.getElementById('newset4').value = doc.data().sets
+        document.getElementById('newset5').value = doc.data().reps
+        document.getElementById('newset6').value = doc.data().rir
+        document.getElementById('newset7').value = doc.data().day
+    })
+
+    $('#editmodal').modal('toggle')
+
+
+
+}
+
+function edit(id) {
+
+    db.collection('sets').doc(id).update({
+        name: document.getElementById('newset1').value,
+        notes: document.getElementById('newset2').value,
+        rest: document.getElementById('newset3').value,
+        sets: document.getElementById('newset4').value,
+        reps: document.getElementById('newset5').value,
+        rir: document.getElementById('newset6').value,
+        day: document.getElementById('newset7').value,
+    }).then(function () {
+        Snackbar.show({ text: 'Updated workout.' });
+        refresh()
+    })
+
+}
+
+function deletething(id) {
+
+    x = confirm('Are you sure you want to delete the workout with the ID: ' + id)
+
+    if (x) {
+        db.collection('sets').doc(id).delete().then(function () {
+            Snackbar.show({ text: 'Workout deleted.' });
+            refresh()
+        })
+    }
+    else {
+
+    }
+
+
+
+}
+
+
+function loadsets() {
+
+    db.collection('sets').get().then(function (querySnapshot) {
+        querySnapshot.forEach(function (doc) {
+
+
+            b = document.createElement('div')
+            b.classList.add('card')
+            b.style.display = 'inline-block'
+            b.style.textAlign = 'left'
+            b.style.width = '80%'
+            editFunc = "prepareedit('" + doc.id + "')"
+            deleteFunc = "deletething('" + doc.id + "')"
+            b.innerHTML = '<div class="card-body"><h3>' + doc.data().name + '</h3><h5>Notes: ' + doc.data().notes + '</h5><h5>Reps: ' + doc.data().reps + '</h5><h5>Rest: ' + doc.data().rest + '</h5><h5>RIR: ' + doc.data().rir + '</h5><h5>Sets: ' + doc.data().sets + '</h5><center><button onclick="' + editFunc + '" class="eon-contained">edit</button><button onclick="' + deleteFunc + '" class="eon-contained">delete</button></center></div>'
+
+            if ($('#' + doc.data().day).length > 0) {
+
+                document.getElementById('content' + doc.data().day).appendChild(b)
+
+            } else {
+
+                a = document.createElement('div')
+                a.id = doc.data().day
+                a.innerHTML = '<div class="card" style="width: 80%; padding: 12px;"><div id="content' + doc.data().day + '" class="card-body"><h3>Day ' + doc.data().day + '</h3></div></div><br><br>'
+
+                document.getElementById('sets').appendChild(a)
+
+
+                document.getElementById('content' + doc.data().day).appendChild(b)
+
+                doSort()
+
+            }
+
+        })
+    })
+
+}
+
+function doSort() {
+    var main = document.getElementById('sets');
+
+    [].map.call(main.children, Object).sort(function (a, b) {
+        return +a.id.match(/\d+/) - +b.id.match(/\d+/);
+    }).forEach(function (elem) {
+        main.appendChild(elem);
+    });
+}
+
--- js/script.js
+++ js/script.js
@@ -30,6 +30,13 @@ firebase.auth().onAuthStateChanged(function (user) {
 
         document.getElementById('signed').style.display = 'block'
         document.getElementById('unsigned').style.display = 'none'
+
+        document.getElementById('username1').innerHTML = user.displayName
+        document.getElementById('useremail1').innerHTML = user.email
+        document.getElementById('pfp1').src = user.photoURL
+
+        loadstuff()
+
     } else {
         document.getElementById('unsigned').style.display = 'block'
         document.getElementById('signed').style.display = 'none'
@@ -77,3 +84,92 @@ var chart = new Chart(ctx, {
     options: {}
 })
 
+
+
+function signout() {
+    Snackbar.show({ text: 'Signing out...' })
+    window.setTimeout(function () {
+        firebase.auth().signOut().then(function () {
+            Snackbar.show({ text: 'Signed out.' })
+        }).catch(function (error) {
+            Snackbar.show({ text: 'Error signing out: ' + error })
+        });
+    }, 2500)
+}
+
+function openworkout(id) {
+    $('#workoutmodal').modal('toggle')
+
+    db.collection('sets').doc(id).get().then(function (doc) {
+
+        document.getElementById('workoutname').innerHTML = doc.data().name
+        document.getElementById('workoutdescription').innerHTML = doc.data().notes
+        document.getElementById('reps').innerHTML = doc.data().reps
+        document.getElementById('sets').innerHTML = doc.data().sets
+        document.getElementById('rest').innerHTML = doc.data().sets
+
+
+    })
+}
+
+function loadstuff() {
+
+    db.collection('sets').get().then(function (querySnapshot) {
+        querySnapshot.forEach(function (doc) {
+
+
+            b = document.createElement('div')
+            b.classList.add('grid-item')
+            workoutfunc = "openworkout('" + doc.id + "')"
+            b.innerHTML = '<button onclick="' + workoutfunc + '" class="eon-outlined">' + doc.data().name + '</button>'
+
+            if ($('#' + doc.data().day).length > 0) {
+
+                document.getElementById('content' + doc.data().day).appendChild(b)
+
+            } else {
+
+                a = document.createElement('div')
+                a.id = doc.data().day
+                a.innerHTML = '<div class="card"><center><h3>Day ' + doc.data().day + '</h3></center>     <div style="width: 90%; border: 1px solid rgb(133, 133, 133);" id="content' + doc.data().day + '" class="grid-container">     </div> <br></div><br><br>'
+
+                document.getElementById('workouts').appendChild(a)
+
+
+                document.getElementById('content' + doc.data().day).appendChild(b)
+
+                doSort()
+
+            }
+
+        })
+    })
+
+}
+
+function doSort() {
+    var main = document.getElementById('workouts');
+
+    [].map.call(main.children, Object).sort(function (a, b) {
+        return +a.id.match(/\d+/) - +b.id.match(/\d+/);
+    }).forEach(function (elem) {
+        main.appendChild(elem);
+    });
+}
+
+/*<div class="card">
+  <center><h3>Day 1</h3></center>
+    <div style="width: 90%; border: 1px solid rgb(133, 133, 133);" class="grid-container">
+    </div>
+<br>
+  </div>*/
+
+/*<div class="card">
+  <center><h3>Day 1</h3></center>
+    <div style="width: 90%; border: 1px solid rgb(133, 133, 133);" class="grid-container">
+      <div class="grid-item">
+        <button class="eon-outlined">Name of workout</button>
+      </div>
+    </div>
+<br>
+  </div>*/
